da8d54a0b0d2c905c63c5de49e0d9e7f88730417
ArSaN <80469568+nardini-22@users.noreply.github.com>
2021-04-02 14:43:42 -0300

    Delete Aula4 directory

--- Aula4/Modelo/src/scripts/script.js
+++
@@ -1,5 +0,0 @@
-var listaFilme = ["https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg" , "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg" , "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg"]
-
-for(i = 0; i < listaFilme.length; i++){
-    document.write("<img src =" + listaFilme[i] + ">")
-}
