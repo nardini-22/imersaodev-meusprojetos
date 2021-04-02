2ccf380462450bffb42fa5110d5d69b2660e36a1
ArSaN <80469568+nardini-22@users.noreply.github.com>
2021-04-02 14:43:19 -0300

    Delete Aula1 directory

--- Aula1/Desafio1/src/scripts/script.js
+++
@@ -1,6 +0,0 @@
-//Transforma dólar em real
-var dolar = parseFloat(prompt("Insira o valor, em reais, que você deseja converter em doláres: "))
-alert("Valor em dólar inserido: " + dolar)
-
-var real = dolar * 5.52
-alert("Valor em real R$" + real.toFixed(2)) //to.Fixed() coloca x casas decimais
