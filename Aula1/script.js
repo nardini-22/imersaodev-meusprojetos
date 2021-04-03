fb43bc37937a0236887c146083dea4e6b1908ffc
ArSaN <80469568+nardini-22@users.noreply.github.com>
2021-04-03 00:42:53 -0300

    Add files via upload

---
+++ Aula1/script.js
@@ -0,0 +1,30 @@
+function converter(opcao){
+    var input = parseFloat(document.querySelector('#valor').value)
+    var divResultado = document.querySelector('#resultado')
+    const dolar = 5.71
+    const euro = 6.71
+    const libra = 7.89
+    const bitcoin = 338141
+    var result
+    switch(opcao){
+        case 'dolar':
+            var result = input / dolar 
+            result = result.toFixed(2)
+            break;
+        case 'euro':
+            var result = input / euro
+            result = result.toFixed(2)
+            break;
+        case 'libra':
+            var result = input / libra
+            result = result.toFixed(2)
+            break;
+        case 'bitcoin':
+            var result = input  / bitcoin
+            break;
+    }
+    if(input){
+        divResultado.innerHTML = "<p>Valor inserido: " + input + "</p>" 
+        divResultado.innerHTML += "<p>Valor da conversão: " + result + "</p>"   
+    }
+}
